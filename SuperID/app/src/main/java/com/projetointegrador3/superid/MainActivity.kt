51cdf1975986d93d2eb73afb9509c7a768858929
Daniel Henrique Inoue Jange <daniel.hij@puccampinas.edu.br>
2025-04-01 18:33:59 -0300

    create account

--- SuperID/app/src/main/java/com/projetointegrador3/superid/MainActivity.kt
+++ SuperID/app/src/main/java/com/projetointegrador3/superid/MainActivity.kt
@@ -4,44 +4,98 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 import com.projetointegrador3.superid.ui.theme.SuperIDTheme
 
 class MainActivity : ComponentActivity() {
+    private lateinit var auth: FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
+        auth = Firebase.auth
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             SuperIDTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                SuperIDApp()
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun createAccount(email: String, password:String){
+    val auth = Firebase.auth
+    auth.createUserWithEmailAndPassword(email, password)
+        .addOnCompleteListener { task ->
+            if (task.isSuccessful) {
+                // Conta criada com sucesso
+                println("Usuário criado com sucesso!")
+            } else {
+                // Erro ao criar a conta
+                println("Erro ao criar conta: ${task.exception?.message}")
+            }
+        }
 }
 
-@Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun SignUpForm(modifier: Modifier = Modifier) {
+    var email by remember { mutableStateOf("") }
+    var password by remember { mutableStateOf("") }
+
+    Column (
+        modifier = modifier,
+        horizontalAlignment = Alignment.CenterHorizontally,
+    ) {
+        Spacer(modifier = Modifier.height(64.dp))
+
+        TextField(
+            value = email,
+            onValueChange = { email = it },
+            label = { Text("Digite o nome email") },
+            modifier = Modifier.fillMaxWidth()
+        )
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        TextField(
+            value = password,
+            onValueChange = { password = it },
+            label = { Text("Digite a senha") },
+            modifier = Modifier.fillMaxWidth()
+        )
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        Button(onClick = { createAccount(email, password) }, modifier = Modifier.fillMaxWidth()){
+            Text(text = "Criar")
+        }
+    }
+}
+
+@Preview
+@Composable
+fun SuperIDApp() {
     SuperIDTheme {
-        Greeting("Android")
+        SignUpForm()
     }
 }
