476c137dc4420b6a8413aaac924e16d168ff71d3
WilliamPuc01 <william.kn@puccampinas.edu.br>
2025-04-03 23:09:20 -0300

    criando outra activity

--- SuperID/app/src/main/java/com/projetointegrador3/superid/MainActivity.kt
+++ SuperID/app/src/main/java/com/projetointegrador3/superid/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.projetointegrador3.superid
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -10,7 +11,9 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.material3.Button
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
@@ -21,8 +24,10 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.firestore.ktx.firestore
@@ -57,20 +62,12 @@ fun createAccount(email: String, password:String){
         }
 }
 
-fun login(email: String, password:String){
-    val auth = Firebase.auth
-    auth.signInWithEmailAndPassword(email, password)
-        .addOnCompleteListener { task ->
-            if (task.isSuccessful) {
-                println("Login realizado com sucesso!")
-            } else {
-                println("Erro ao fazer login: ${task.exception?.message}")
-            }
-        }
-}
+
 
 @Composable
-fun SignUpForm(modifier: Modifier = Modifier) {
+fun SignUpForm(modifier: Modifier = Modifier
+    .fillMaxSize()
+    .wrapContentSize(Alignment.Center)) {
     var email by remember { mutableStateOf("") }
     var password by remember { mutableStateOf("") }
 
@@ -78,9 +75,11 @@ fun SignUpForm(modifier: Modifier = Modifier) {
         modifier = modifier,
         horizontalAlignment = Alignment.CenterHorizontally,
     ) {
-        Spacer(modifier = Modifier.height(64.dp))
+       // Spacer(modifier = Modifier.height(64.dp))
+        Text("Cadastro de Usuario", fontSize = 30.sp)
+        Spacer(modifier = Modifier.height(20.dp))
 
-        TextField(
+        OutlinedTextField(
             value = email,
             onValueChange = { email = it },
             label = { Text("Digite o email") },
@@ -89,7 +88,7 @@ fun SignUpForm(modifier: Modifier = Modifier) {
 
         Spacer(modifier = Modifier.height(16.dp))
 
-        TextField(
+        OutlinedTextField(
             value = password,
             onValueChange = { password = it },
             label = { Text("Digite a senha") },
@@ -102,9 +101,14 @@ fun SignUpForm(modifier: Modifier = Modifier) {
             Text(text = "Criar")
         }
 
-        Button(onClick = { login(email, password) }, modifier = Modifier.fillMaxWidth()){
-            Text(text = "Entrar")
+        //Forma de mudar para activity sem precisar criar arquivo xml
+        val context = LocalContext.current
+        Button(onClick = {
+            val intent = Intent(context, MainActivity2::class.java)
+            context.startActivity(intent)},  modifier = Modifier.fillMaxWidth()) {
+            Text(text = "Fazer Login")
         }
+
     }
 }
 
---
+++ SuperID/app/src/main/java/com/projetointegrador3/superid/MainActivity2.kt
@@ -0,0 +1,93 @@
+package com.projetointegrador3.superid
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.material3.Button
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.google.firebase.Firebase
+import com.google.firebase.auth.auth
+import com.projetointegrador3.superid.ui.theme.SuperIDTheme
+
+class MainActivity2 : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            SuperIDTheme {
+                LoginApp()
+            }
+        }
+    }
+}
+
+
+@Preview
+@Composable
+fun LoginApp(){
+    LoginForm()
+}
+fun login(email: String, password:String){
+    val auth = Firebase.auth
+    auth.signInWithEmailAndPassword(email, password)
+        .addOnCompleteListener { task ->
+            if (task.isSuccessful) {
+                println("Login realizado com sucesso!")
+            } else {
+                println("Erro ao fazer login: ${task.exception?.message}")
+            }
+        }
+}
+
+
+
+@Composable
+fun LoginForm(modifier: Modifier = Modifier
+    .fillMaxSize()
+    .wrapContentSize(Alignment.Center)){
+    var email by remember { mutableStateOf("") }
+    var senha by remember {mutableStateOf("")}
+
+    Column (
+        modifier = modifier,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text("Login", fontSize = 30.sp)
+        Spacer(modifier = Modifier.height(20.dp))
+        OutlinedTextField(
+            value = email,
+            onValueChange = { email = it },
+            label = { Text("Email") }
+        )
+        Spacer(modifier = Modifier.height(10.dp))
+        OutlinedTextField(
+            value = senha,
+            onValueChange = { senha = it },
+            label = { Text("Senha") }
+        )
+        Spacer(modifier = Modifier.height(10.dp))
+        Button(onClick = { login(email, senha) }) {
+            Text(text = "Entrar")
+        }
+    }
+}
