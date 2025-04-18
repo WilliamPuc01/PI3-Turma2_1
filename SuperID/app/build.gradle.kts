874bab6158c30453ac17d11718527e4bb17b316b
unknown <lukinhasgondim7@gmail.com>
2025-04-18 19:00:41 -0300

    implementação do layout avançado da home

--- SuperID/app/build.gradle.kts
+++ SuperID/app/build.gradle.kts
@@ -59,6 +59,8 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+    implementation("androidx.compose.material3:material3:1.2.0")
+
 
     // Firebase
     implementation(platform("com.google.firebase:firebase-bom:33.12.0"))
--- SuperID/app/src/main/java/com/projetointegrador3/superid/HomeActivity.kt
+++ SuperID/app/src/main/java/com/projetointegrador3/superid/HomeActivity.kt
@@ -1,3 +1,4 @@
+
 package com.projetointegrador3.superid
 
 import android.content.Intent
@@ -5,26 +6,56 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.*
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.materialIcon
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.MoreVert
+import androidx.compose.material.icons.filled.Settings
 import androidx.compose.material3.Button
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.ExtendedFloatingActionButton
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.MediumTopAppBar
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SearchBar
+import androidx.compose.material3.SmallTopAppBar
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.Alignment.Companion.CenterVertically
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -33,7 +64,28 @@ import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
+import androidx.compose.material.icons.filled.Search
 import com.projetointegrador3.superid.ui.theme.SuperIDTheme
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.filled.AccountCircle
+import androidx.compose.material.icons.filled.Add
+
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Lock
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
 
 
 
@@ -44,32 +96,146 @@ class HomeActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             SuperIDTheme {
-                HomeScreenPreview()
+                HomeScreen()
             }
         }
     }
 }
 
-@Preview
+
+
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun HomeScreenPreview() {
-    SuperIDTheme {
-        HomeScreen()
+@Preview
+
+fun HomeScreen() {
+    // Só alterar aqui se quiser mudar o tema do app!!!
+
+    val customColors = darkColorScheme(
+        primary = Color(0xFFD4AF37), // Dourado
+        surface = Color(0xFF121212), // Preto
+        onSurface = Color.White
+    )
+
+    MaterialTheme(colorScheme = customColors) {
+        //ESTRUTURA BASICA DA TELA HOME DO APP
+        Scaffold(
+            modifier = Modifier.fillMaxSize(),
+
+            //cabeçalho com o logo do app
+            topBar = {
+                Column {
+                    Image(
+                        painter = painterResource(R.drawable.superid_removebg),
+                        contentDescription = "Logo SuperID",
+                        modifier = Modifier
+                            .height(124.dp)
+                            .align(Alignment.CenterHorizontally)
+                            .padding(2.dp)
+                    )
+                    Text(
+                        "Senhas",
+                        style = MaterialTheme.typography.titleLarge,
+                        modifier = Modifier.padding(
+                            horizontal = 16.dp,
+                        )
+                    )
+                }
+
+            }, floatingActionButton = {
+                FloatingActionButton(
+                    onClick = { /* Todo: funcionalide de adicionar senhas */ },
+                    containerColor = MaterialTheme.colorScheme.primary,
+                    contentColor = Color.Black
+                ) {
+                    Icon(
+                        Icons.Default.Add,
+                        contentDescription = "Adicionar senha",
+                        modifier = Modifier.size(24.dp)
+                    )
+                }
+            }
+        )
+        
+        //CONTEUDO PRINCIPAL
+        { padding ->
+
+            Box(
+                modifier = Modifier.fillMaxSize()
+            ) {
+                // Imagem de fundo
+                Image(
+                    painter = painterResource(R.drawable.fundo),
+                    contentDescription = "Fundo do app",
+                    contentScale = ContentScale.Crop,
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .alpha(0.2f)
+                )}
+            // Grade das categorias num formato 2x2
+            LazyVerticalGrid(
+                columns = GridCells.Fixed(2),
+                modifier = Modifier.padding(padding),
+                contentPadding = PaddingValues(16.dp),
+                verticalArrangement = Arrangement.spacedBy(16.dp),
+                horizontalArrangement = Arrangement.spacedBy(16.dp)
+            ) {
+                //transforma os itens da lista de categorias em um card
+                items(categories) { category ->
+                    Cards(category)
+                }
+            }
+        }
     }
 }
 
+// Categorias do app e seus icones, só add aqui se quiser uma nova
+val categories = listOf(
+    "Sites Web" to Icons.Default.Email,
+    "Aplicativos" to Icons.Default.Favorite,
+    "Teclados Físicos" to Icons.Default.Menu,
+    "Códigos" to Icons.Default.Lock
+)
 
 
+//Definição dos CARDS
 @Composable
-fun HomeScreen(modifier: Modifier = Modifier) {
+fun Cards(category: Pair<String, ImageVector>) {
+    Card(
 
-    Column(
-        modifier = modifier.fillMaxSize(),
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally
-    ) {
-        Text(
-            "Bem-Vindo!!!!! "
+        modifier = Modifier.clickable { /* navegação */ }
+            .size(160.dp) // Tamanho quadrado
+            .padding(8.dp),
+        colors = CardDefaults.cardColors(
+            containerColor = MaterialTheme.colorScheme.surface,
+            contentColor = MaterialTheme.colorScheme.onSurface
+        ),
+        elevation = CardDefaults.cardElevation(
+            defaultElevation = 8.dp,
+            pressedElevation = 4.dp
         )
+    )
+        //Conteudo do CARD
+        {
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(16.dp),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Icon(
+                imageVector = category.second,
+                contentDescription = null,
+                tint = MaterialTheme.colorScheme.primary,
+                modifier = Modifier.size(40.dp)
+            )
+            Spacer(modifier = Modifier.height(16.dp))
+            Text(
+                text = category.first,
+                style = MaterialTheme.typography.titleMedium,
+                textAlign = TextAlign.Center
+            )
+        }
     }
 }
